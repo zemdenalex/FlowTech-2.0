494a99357f6c1c5a8b89177cb9572892661969f4
Denis <zemdenalex@gmail.com>
2025-10-05 21:19:07 +0000

    added fasie logo

--- site/src/components/Contacts/Contacts.jsx
+++ site/src/components/Contacts/Contacts.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import fasie from '/assets/fasie.png'
 const Contacts = () => {
   return (
     <div>
@@ -8,12 +8,18 @@ const Contacts = () => {
           КОНТАКТЫ
         </h2>
       </div>
-      <div className='container max-md:my-10 md:my-20'>
+      <div className='container max-md:my-10 md:my-20 flex max-md:flex-col max-md:items-center h-auto flex-row justify-around'>
         <div className='max-md:px-5 md:px-20 rounded-[30px] py-5 md:py-10 bg-gradient-to-br from-gd0 via-gd40 to-gd70 '>
           <p className='text-white max-md:text-xs md:text-[25px] font-regular font-roboto'>
             Тел: +7 (926) 545-73-91<br/>
             E-mail: FlowTechADT@yandex.ru<br/>
             Контактное лицо: Усачев Никита
+          </p>
+        </div>
+        <div className='p-5'>
+          <p className='text-white max-md:text-xs md:text-[25px] max-md:pt-5 max-md:text-center font-regular font-roboto'>
+            При поддержке <b>ФСИ</b>
+            <img className='p-5 object-contain' src={fasie}/>
           </p>
         </div>
       </div>
--- site/src/components/Footer/Footer.jsx
+++ site/src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <div className='w-full py-2 bg-secondary flex justify-center'>
-      <p className='max-md:text-xs md:text-lg'>FlowTech 2024</p>
+      <p className='max-md:text-xs md:text-lg'>© {year} FlowTech. Все права защищены.</p>
     </div>
   )
 }
